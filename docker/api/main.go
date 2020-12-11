26706aeee307c7986307716275f0c34520733307
silmin <silvestmint@gmail.com>
2020-12-11 17:22:38 +0900

    add (current)user method(get/add/delete)

--- docker/api/main.go
+++ docker/api/main.go
@@ -13,7 +13,14 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", service.ListUsers)
+	e.GET("/users", service.GetAllUsers)
+	e.GET("/users/:cardNo", service.GetUser)
+	e.POST("/users/:cardNo", service.RegisterUser)
+	e.DELETE("/users/:cardNo", service.DeleteUser)
+
+	e.GET("/currents", service.GetCurrentUsers)
+	e.PUT("/currents/:cardNo", service.PushCurrentUser)
+	e.DELETE("/currents/:cardNo", service.DeleteCurrentUser)
 
 	e.Start(":80")
 }
--- docker/db/sql/002_insert_sample.sql
+++ docker/db/sql/002_insert_sample.sql
@@ -1,3 +1,6 @@
 insert into users (display_name, full_name, email, card_no) values ("hoge", "Hoge", "hoge@example.com", "1000001");
 insert into users (display_name, full_name, email, card_no) values ("fuga", "Fuga", "fuga@example.com", "1000002");
 insert into users (display_name, full_name, email, card_no) values ("foo", "Foo", "foo@example.com", "1000003");
+
+insert into current_users (card_no) values ("1000001");
+insert into current_users (card_no) values ("1000002");
