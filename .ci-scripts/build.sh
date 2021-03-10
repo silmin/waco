617f0c2e1154f3dd612c4402e92b6c0ff854b592
doariva <b1816123@planet.kanazawa-it.ac.jp>
2021-03-10 18:56:28 +0900

    fix docker build path again

--- .ci-scripts/build.sh
+++ .ci-scripts/build.sh
@@ -1,7 +1,7 @@
 mkdir -p /kaniko/.docker
 
 # docker build & push
-/kaniko/executor --context $CI_PROJECT_DIR/docker/api/Dockerfile \
+/kaniko/executor --context $CI_PROJECT_DIR/docker/api/ \
 	--dockerfile $CI_PROJECT_DIR/docker/api/Dockerfile \
 	--destination $INTERNAL_REGISTRY/$REGISTRY_REPO:$CI_COMMIT_TAG \
 	--destination $INTERNAL_REGISTRY/$REGISTRY_REPO:$CI_COMMIT_SHA \
